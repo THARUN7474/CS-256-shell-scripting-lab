f15dd2908757ffb1b1c89b27a7a159df8cdd2bbb
THARUN7474 <bandatharun74@gmail.com>
2024-04-14 23:49:24 +0530

    added assign8

---
+++ assignment 8/nowcommands.sh
@@ -0,0 +1,34 @@
+PS C:\Users\Tharun\Desktop\unixlab\SHELL-SCRIPTING-LAB> git status
+On branch main
+Your branch is up to date with 'origin/main'.
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+        assignment 8/
+
+nothing added to commit but untracked files present (use "git add" to track)
+PS C:\Users\Tharun\Desktop\unixlab\SHELL-SCRIPTING-LAB> git add .
+PS C:\Users\Tharun\Desktop\unixlab\SHELL-SCRIPTING-LAB> git commit -m "added assign8"
+[main 5e2cad4] added assign8
+ 1 file changed, 0 insertions(+), 0 deletions(-)
+ create mode 100644 assignment 8/commands.pdf
+PS C:\Users\Tharun\Desktop\unixlab\SHELL-SCRIPTING-LAB> git push
+Enumerating objects: 5, done.
+Counting objects: 100% (5/5), done.
+Delta compression using up to 12 threads
+Compressing objects: 100% (3/3), done.
+Writing objects: 100% (4/4), 1.26 MiB | 656.00 KiB/s, done.
+Total 4 (delta 1), reused 0 (delta 0), pack-reused 0
+remote: Resolving deltas: 100% (1/1), completed with 1 local object.
+To https://github.com/THARUN7474/SHELL-SCRIPTING-LAB.git
+   36c79ee..5e2cad4  main -> main
+PS C:\Users\Tharun\Desktop\unixlab\SHELL-SCRIPTING-LAB> 
+
+
+
+
+git status
+git add .
+
+git commit -m "added assign8"
+git push
